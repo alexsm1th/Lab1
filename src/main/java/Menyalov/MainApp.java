a894d073d9e34e18d9c0fbeb38144e088517fb0c
alexsm1th <rainbow2350light@gmail.com>
2019-11-01 16:22:24 +0200

    Update MainApp.java

--- src/main/java/Menyalov/MainApp.java
+++ src/main/java/Menyalov/MainApp.java
@@ -5,8 +5,7 @@ import javax.swing.JFrame;
 
 public class MainApp {
     public static void main(String... args) throws Exception {
-        //эта функция может быть и в другом классе
-        MyWindowApp app = new MyWindowApp(); //Создаем экземпляр нашего приложения
-        app.setVisible(true); //С этого момента приложение запущено!
+        MyWindowApp app = new MyWindowApp();
+        app.setVisible(true);
     }
 }
